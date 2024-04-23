c6b25debd043d359dc4d3470a0b7d99873add38a
t_rident <gabrieldavanco4@gmail.com>
2024-04-23 20:15:02 -0300

    Jogo Pronto - FlappyBird Maneirissimo
    Jogo com:
    -Música
    -HighScore
    -Todas as funcionalidade de flappy bird

--- Estudos para N1 - Jogo em duas aulas/Assets/Scripts/infiniteMap.cs
+++ Estudos para N1 - Jogo em duas aulas/Assets/Scripts/infiniteMap.cs
@@ -1,14 +1,74 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
 
 public class infiniteMap : MonoBehaviour
 {
     public GameObject pipeSection;
+    public TMP_Text title;
+    public TMP_Text pontuacao;
+    public TMP_Text recorde;
+
+    int pontos;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+        CheckBestScoreText();
+  }
 
     private void OnTriggerEnter(Collider other)
     {
+        float pos = Random.Range(3.0f, 0.1f);
+
         if (other.gameObject.CompareTag("invisible_wall"))
-            Instantiate(pipeSection, new Vector3(2f, 1f, -8f), Quaternion.identity);
+        {
+            Instantiate(pipeSection, new Vector3(2f, pos, -8f), Quaternion.identity);
+            title.enabled = false;
+        }
+
+
+        if (other.gameObject.CompareTag("points"))
+        {
+            pontos++;
+            pontuacao.SetText(pontos.ToString());
+            CheckBestScore();
+            CheckBestScoreText();
+        }
     }
+
+
+    void CheckBestScore()
+    {
+        if (pontos > PlayerPrefs.GetInt("Best", 0))
+            PlayerPrefs.SetInt("Best", pontos);
+
+        
+    }
+
+    void CheckBestScoreText()
+    {
+        recorde.text = $"{PlayerPrefs.GetInt("Best", 0)}";
+    }
+
+
+
+    private void Reset()
+    {
+        SceneManager.LoadScene("Flappy");
+    }
+
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("pipes"))
+            Reset();
+    }
+
 }
